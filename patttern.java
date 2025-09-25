149053955e12a44159f6eca968878b782be52498
mohanapriya-120705 <priyapriya01451@gmail.com>
2025-09-25 14:46:44 +0530

    Create patttern.java

---
+++ patttern.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+  public static void main(String[]args) {
+    for(int i=0;i<3;i++) {
+      System.out.print("*");
+    }
+    System.out.println();
+    for(int i=0;i<3;i++) {
+      System.out.println(" *");
+    }
+    for(int i=0;i<7;i++) {
+      System.out.print("*");
+    }
+  }
+}
