94d64736c4a337c1c53da6ee1b49d4addbd5023d
mohanapriya-120705 <priyapriya01451@gmail.com>
2025-09-30 12:38:49 +0530

    Create print a pyramid pattern.java

---
+++ print a pyramid pattern.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+public static void main(String[] args) {
+  Scanner sc = new Scanner(System.in);
+  System.out.print("Enter number of rows: ");
+  int n = sc.nextInt();
+  for(int i=1; i<=n; i++) {
+    for(int j=i; j<n; j++) System.out.print(" ");
+    for(int k=1; k<=2*i-1; k++) System.out.print("*");
+    System.out.println();
+  }
+  sc.close();
+}
+}
